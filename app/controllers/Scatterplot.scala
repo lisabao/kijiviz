a4318335b8562d0d6d8dca6b0d1e4a65715345d3
lisabao <lisa.y.bao@gmail.com>
2013-06-26 14:42:15 -0700

    More work on Scatterplot.

--- app/controllers/Scatterplot.scala
+++ app/controllers/Scatterplot.scala
@@ -1,9 +1,12 @@
 package org.kiji.kijiviz //TODO double-check this; add copyright top comment?
 
+import breeze.plot._
+
 import org.kiji.schema.Kiji
 import org.kiji.schema.KijiDataRequest
 import org.kiji.schema.KijiRowData
 import org.kiji.schema.KijiRowScanner
+import org.kiji.schema.KijiScannerOptions
 import org.kiji.schema.KijiTable
 import org.kiji.schema.KijiTableReader
 import org.kiji.schema.KijiURI
@@ -22,19 +25,42 @@ class Scatterplot (
     columnX: String, columnY: String,
     rowStart: Int, rowEnd: Int) {
 
-  private val myTableURI = "kiji://.env/default/" //for now, use this default table
-
-  //basic testing
-  def testing() {
-    getKijiTableData(myTableURI, "tempo", "duration", 1, 50)
-  }
-
   //Get the specified data out of a Kiji table and return it as a Breeze dataset.
   //TODO what is the needed type of a Breeze dataset?
   def getKijiTableData(tableURI, columnX, columnY, rowStart, rowEnd) = {
     Kiji kiji = Kiji.Factory.open(tableURI)
+    KijiTable kijiTable = kiji.openTable("tableName")
 
+    // Define a KDR.
+    KijiDataRequestBuilder builder = KijiDataRequest.builder
+        .newColumnsDef
+        .withMaxVersions(1)
+        .add(columnX, columnY)
+    KijiDataRequest request = builder.build
 
-    kiji.release() //Finished with the Kiji table, yay!
+    // Get a scanner to iterate over rows.
+    KijiTableReader reader = kijiTable.openTableReader
+    KijiScannerOptions options = new KijiScannerOptions()
+        .setStartRow(rowStart)
+        .setEndRow(rowEnd)
+    KijiRowScanner scanner = reader.getScanner(request, options)
+
+    // Process KijiRowData into Breeze dataset.
+    fromKijiToBreeze(scanner)
+
+    reader.close
+    kiji.release //Finished with the Kiji table, yay!
   }
+
+  def fromKijiToBreeze(scanner: KijiRowScanner) = {
+    try {
+      scanner.foreach { row: KijiRowData =>
+          //do stuff to each row
+      }
+    }
+    finally {
+      scanner.close
+    }
+  }
+
 }
